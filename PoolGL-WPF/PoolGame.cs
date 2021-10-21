61cebe6424239f592f89408282835b041f143342
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-21 14:59:38 +0200

    base arrangement

--- PoolGL-WPF/PoolGame.cs
+++ PoolGL-WPF/PoolGame.cs
@@ -12,36 +12,55 @@ namespace PoolGL_WPF
 {
     public sealed class PoolGame : GameBase
     {
-        public PoolTable Table;
-        public PlayBall PlayBall;
-        public Player Player;
-        public PoolBall[] PoolBalls;
+        public readonly PoolBall[] PoolBalls = new PoolBall[15];
+        public readonly PoolTable Table;
+        public readonly PlayBall PlayBall;
+        public readonly Player Player;
         public override ITransform Camera { get; } = new Singularity(Vector3.UnitZ * 50);
         public Vector2 MousePosition { get; internal set; }
 
         public PoolGame()
         {
             AddChild(Table = new PoolTable());
-            AddChild(PlayBall = new PlayBall(new Singularity(Vector3.UnitX*-27)));
-            AddChild(new PoolBall(new Singularity(Vector3.UnitX*-13), 1));
-            AddChild(new PoolBall(new Singularity(Vector3.UnitY*6), 2));
+            AddChild(PlayBall = new PlayBall(new Singularity(Vector3.UnitX*-25)));
+            for (byte i = 1; i < 16; i++)
+                AddChild(PoolBalls[i-1]=new PoolBall(new Singularity(Vector3.UnitX*20), i));
             AddChild(Player = new Player(PlayBall, new Singularity()));
+
+            ArrangeCluster();
+        }
+
+        private void ArrangeCluster()
+        {
+            var rad = PlayBall.Scale.X * 2 + 0.01f;
+            var off = Vector3.UnitX * rad;
+            int angle = 120;
+            var turn1 = Quaternion.CreateFromAxisAngle(Vector3.UnitZ, angle);
+            var turn2 = Quaternion.CreateFromAxisAngle(Vector3.UnitZ, -angle);
+            var off1 = Vector3.Transform(off, turn1);
+            var off2 = Vector3.Transform(off, turn2);
+            var pos = PoolBalls[0].Position;
+
+            PoolBalls[1].Transform.Position = off1 * 1 + pos;
+            PoolBalls[2].Transform.Position = off1 * 2 + pos;
+            PoolBalls[3].Transform.Position = off1 * 3 + pos;
+            PoolBalls[4].Transform.Position = off1 * 4 + pos;
+            PoolBalls[5].Transform.Position = off2 * 1 + off1 * 0 + pos;
+            PoolBalls[6].Transform.Position = off2 * 1 + off1 * 2 + pos;
+            PoolBalls[7].Transform.Position = off1 + off2 + pos;
+            PoolBalls[8].Transform.Position = off2 * 1 + off1 * 3 + pos;
+            PoolBalls[9].Transform.Position = off2 * 2 + off1 * 0 + pos;
+            PoolBalls[10].Transform.Position = off2 * 2 + off1 * 1 + pos;
+            PoolBalls[11].Transform.Position = off2 * 2 + off1 * 2 + pos;
+            PoolBalls[12].Transform.Position = off2 * 3 + off1 * 0 + pos;
+            PoolBalls[13].Transform.Position = off2 * 3 + off1 * 1 + pos;
+            PoolBalls[14].Transform.Position = off2 * 4 + pos;
         }
 
         protected override bool _Load()
         {
             BaseTickTime = 2000;
             return base._Load();
-        }
-
-        protected override bool _Enable()
-        {
-            return base._Enable();
-        }
-
-        protected override void _Disable()
-        {
-            base._Disable();
         }
 
         public void Shoot(float strength)
