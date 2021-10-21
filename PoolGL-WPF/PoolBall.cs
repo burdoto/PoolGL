19b64f52d53e336fcfcb42766683574f9243ac73
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-21 17:36:26 +0200

    yea uh

--- PoolGL-WPF/PoolBall.cs
+++ PoolGL-WPF/PoolBall.cs
@@ -17,9 +17,38 @@ namespace PoolGL_WPF
         public PoolBall(Singularity transform, byte fig) : base(transform, fig)
         {
             RenderObjects.Add(new PoolBallCircle(this));
+            /*
+            if (Half)
+                RenderObjects.Add(new Circle(this, new DeltaTransform(this){ScaleDelta = Vector3.One * 0.5f})
+                {
+                    PostBegin = gl => gl.Color(200, 200, 200)
+                });
+                */
+            if (fig != 255)
+                RenderObjects.Add(new Text(this)
+                {
+                    Content = fig.ToString(),
+                    FontColor = System.Drawing.Color.Black
+                });
             Collider = new CircleCollider(this);
             PhysicsObject = new PhysicsObject(this){Inertia = 0.93f};
         }
+
+        public bool Half => Metadata != 255 && Metadata / 8 % 2 > 0.98;
+
+        public Color Color => Metadata == 255 ? White 
+            : (Metadata % 8) switch
+            {
+                0 => Black,
+                1 => Yellow,
+                2 => CornflowerBlue,
+                3 => Red,
+                4 => Purple,
+                5 => Orange,
+                6 => SeaGreen,
+                7 => Brown,
+                _ => throw new ArgumentOutOfRangeException()
+            };
     }
 
     public class PoolBallCircle : AbstractRenderObject
@@ -37,17 +66,17 @@ namespace PoolGL_WPF
 
         public Color Color => GameObject.Metadata == 255 ? White 
             : (GameObject.Metadata % 8) switch
-        {
-            0 => Black,
-            1 => Yellow,
-            2 => CornflowerBlue,
-            3 => Red,
-            4 => Purple,
-            5 => Orange,
-            6 => SeaGreen,
-            7 => Brown,
-            _ => throw new ArgumentOutOfRangeException()
-        };
+            {
+                0 => Black,
+                1 => Yellow,
+                2 => CornflowerBlue,
+                3 => Red,
+                4 => Purple,
+                5 => Orange,
+                6 => SeaGreen,
+                7 => Brown,
+                _ => throw new ArgumentOutOfRangeException()
+            };
 
         public override void Draw(OpenGL gl, ITransform camera) => _base.Draw(gl, camera);
     }
