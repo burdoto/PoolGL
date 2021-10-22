8aa3e310ddfe0a25a195c29649a8cae47319e385
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-22 12:11:04 +0200

    zeug

--- PoolGL-WPF/PoolTable.cs
+++ PoolGL-WPF/PoolTable.cs
@@ -4,6 +4,7 @@ using OpenGL_Util;
 using OpenGL_Util.Game;
 using OpenGL_Util.Model;
 using OpenGL_Util.Physics;
+using OpenGL_Util.Shape2;
 using OpenGL_Util.Shape3;
 using SharpGL;
 
@@ -12,10 +13,18 @@ namespace PoolGL_WPF
     public sealed class PoolTable : AbstractGameObject
     {
         public static readonly Vector3 BaseAreaScale = new Vector3(2, 1, 0.1f);
+        private readonly Text txt;
+
         public PoolTable() : base(new Singularity(-Vector3.UnitZ, (Vector3.UnitX + Vector3.UnitY) * 35))
         {
             RenderObjects.Add(new Cuboid(this, Color.DarkGreen));
+            RenderObjects.Add(txt = new Text(this, new DeltaTransform(this){ PositionDelta = new Vector3(20, 60, 0) }));
             Collider = new InverseCollider(new RectCollider(this));
+        }
+
+        protected override void _Tick()
+        {
+            txt.Content = ((GameBase.Main as PoolGame)?.PlayBall.Position).ToString();
         }
 
         public override Vector3 Scale => base.Scale * BaseAreaScale;
